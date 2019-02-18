0409f19ccdd4a7a7e8bb374d16504d8b8dc1cecc
Carol Sandi <caroladrisf@gmail.com>
2019-02-17 21:33:21 -0600

    Creo las funciones para el CRUD de channels

--- chat-twilio/src/app/services/channel.service.ts
+++ chat-twilio/src/app/services/channel.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/env';
-import { ChannelsArray } from '../models/channel';
+import { ChannelsArray, Channel } from '../models/channel';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,16 +10,42 @@ import { Observable } from 'rxjs';
 })
 export class ChannelService {
 
-  private headers = new HttpHeaders();
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {
-    this.headers = this.headers.append('Content-Type', 'application/json');
-    this.headers = this.headers.append('Authorization', 'Basic '
-    + btoa(`${environment.API_KEY}:${environment.API_SECRET}`));
+  private headersGet() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Authorization', 'Basic ' + btoa(`${environment.API_KEY}:${environment.API_SECRET}`));
+    return headers;
   }
 
-  getChannels(): Observable<ChannelsArray> {
-    const service = localStorage.getItem('service_sid');
-    return this.http.get<ChannelsArray>(`${environment.API_URI}/Services/${service}/Channels`, { headers: this.headers });
+  private headersPost() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers = headers.append('Authorization', 'Basic ' + btoa(`${environment.API_KEY}:${environment.API_SECRET}`));
+    return headers;
   }
+
+  list(service: string): Observable<ChannelsArray> {
+    return this.http.get<ChannelsArray>(`${environment.API_URI}/Services/${service}/Channels`, { headers: this.headersGet() });
+  }
+
+ /*  retrieve(service: string, channel: string): Observable<Channel> {
+    return this.http.get<Channel>(`${environment.API_URI}/Services/${service}/Channels/${channel}`, { headers: this.headersGet() });
+  } */
+
+  create(service: string, friendlyName: string): Observable<Channel> {
+    return this.http.post<Channel>(`${environment.API_URI}/Services/${service}/Channels`, `FriendlyName=${friendlyName}`,
+      { headers: this.headersPost() });
+  }
+
+  /* update(service: string, channel: string, friendlyName: string): Observable<Channel> {
+    return this.http.post<Channel>(`${environment.API_URI}/Services/${service}/Channels/${channel}`, { FriendlyName: friendlyName },
+      { headers: this.headersPost() });
+  } */
+
+  delete(service: string, channel: string) {
+    return this.http.delete(`${environment.API_URI}/Services/${service}/Channels/${channel}`, { headers: this.headersGet() });
+  }
+
 }
